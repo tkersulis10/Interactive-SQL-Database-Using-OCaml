805b157da229597d8c433f341c67702a9fb3b4d7
Tomas <tk492@cornell.edu>
2021-10-12 20:26:18 -0400

    Add specificiations to signature

--- src/main.mli
+++ src/main.mli
@@ -1,11 +1,23 @@
 type db_list
+(** Json list representation of a set of databases. *)
 
 val database_list : string
+(** Convert json database to list of individual databases*)
 
 val dbs_from_file : Yojson.Basic.t
+(** Retrieve json representation of database file. *)
 
 val write_to_file : Yojson.Basic.t -> unit
-
-val add_database : string -> string list -> unit
+(** [write_to_file db] writes [db] to database file. *)
 
 val splice_outer_parens : string -> string
+(** [splice_outer_parens dbm] removes outer parentheses of database
+    management object [dbm] to allow for later insertion of another
+    database inside. Returns with trailing comma if inside parens is not
+    empty. Requires: [dbm] must have length of at least 2, representing
+    the constant 2 outer parentheses of the dbms. *)
+
+val add_database : string -> string list -> unit
+(** [add_database name values] adds a new database labeled [name] with
+    values [values] to the database list -- Yojson.Basic.to_file
+    file_location (Yojson.Basic.from_string str). *)
