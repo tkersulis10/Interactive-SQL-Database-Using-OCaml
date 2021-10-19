a49ee5dd18f79e871dfc4122b17b4bda0f14e3d5
Tomas <tk492@cornell.edu>
2021-10-19 14:47:50 -0400

    Change name from author to authors

---
+++ src/authors.mli
@@ -0,0 +1,6 @@
+(** A SQL database management system.
+
+    @author Max Hadden (mnh38), Tomas Kersulis (tk492) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours you worked on this assignment. *)
