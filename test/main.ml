06a3ae9625fe8654ee4379331b7893b9179f1af4
Tomas <tk492@cornell.edu>
2021-12-06 22:35:12 -0500

    Implement arithmetic functions and testing

--- test/main.ml
+++ test/main.ml
@@ -2,8 +2,6 @@ open OUnit2
 open Database
 open Main
 
-let test_database_helper = dbs_from_file "test_helper_database.json"
-
 let test_database_1 = dbs_from_file "test_database.json"
 
 let test_database_2 = dbs_from_file "test_database2.json"
@@ -141,6 +139,54 @@ let get_fields_list_test
   assert_equal expected_output
     (get_fields_list file database_name)
     ~printer:(String.concat ", ")
+
+(** [sum_of_field_test name file database_name field_name expected_output]
+    creates an OUnit test with name [name] that compares whether
+    [sum_of_field file database_name field_name] is equal to
+    [expected_output]. *)
+let sum_of_field_test
+    (name : string)
+    (file : string)
+    (database_name : string)
+    (field_name : string)
+    (expected_output : float) =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (sum_of_field file database_name field_name)
+    ~printer:string_of_float
+
+(** [mean_of_field_test name file database_name field_name expected_output]
+    creates an OUnit test with name [name] that compares whether
+    [mean_of_field file database_name field_name] is equal to
+    [expected_output]. *)
+let mean_of_field_test
+    (name : string)
+    (file : string)
+    (database_name : string)
+    (field_name : string)
+    (expected_output : float) =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (mean_of_field file database_name field_name)
+    ~printer:string_of_float
+
+(** [computation_of_any_field_test name file database_name field_name element_of_string init computation expected_output]
+    creates an OUnit test with name [name] that compares whether
+    [computation_of_any_field file database_name field_name element_of_string init computation]
+    is equal to [expected_output]. *)
+let computation_of_any_field_test
+    (name : string)
+    (file : string)
+    (database_name : string)
+    (field_name : string)
+    (element_of_string : string -> 'a)
+    (init : 'a)
+    (computation : 'a -> 'a -> 'a)
+    (expected_output : 'a) =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (computation_of_any_field file database_name field_name
+       element_of_string init computation)
 
 (** [update_file_test name file expected_output] creates an OUnit test
     with name [name] that compares whether
@@ -281,6 +327,17 @@ let second_section_tests =
     get_fields_list_test "get_fields_list for test_database2.json"
       "test_database2.json" "Users"
       [ "name"; "age"; "fact"; "Idea" ];
+    computation_of_any_field_test
+      "computation_of_any_field with concatenation of strings with \
+       field name fact in test_database2.json"
+      "test_database2.json" "Users" "fact" identity "hello" ( ^ )
+      "helloCS 3110 is fun.CS 3110 is fun.";
+    computation_of_any_field_test
+      "computation_of_any_field with only one string with that field \
+       name in test_database2.json"
+      "test_database2.json" "test" "2" identity "HI"
+      (fun acc s -> acc ^ s ^ "BYE")
+      "HIzero, 0BYE";
   ]
 
 let third_section_tests =
@@ -389,6 +446,44 @@ let third_section_tests =
       "get_fields_list for test in test_database3.json"
       "test_database3.json" "test"
       [ "1"; "2"; "3"; "tvalue"; "opval" ];
+    sum_of_field_test
+      "sum_of_field for 3 in test in test_database3.json"
+      "test_database3.json" "test" "3" (-2.);
+    mean_of_field_test
+      "mean_of_field for 3 in test in test_database3.json"
+      "test_database3.json" "test" "3" (-0.5);
+    computation_of_any_field_test
+      "computation_of_any_field for 2 in test in test_database3.json"
+      "test_database3.json" "test" "2" int_of_string 0 ( + ) (-1263349);
+  ]
+
+let computation_tests =
+  [
+    sum_of_field_test "sum of int values in computation_database.json"
+      "computation_database.json" "Types" "int" (-47.0);
+    mean_of_field_test "mean of int values in computation_database.json"
+      "computation_database.json" "Types" "int" (-15.6666666666666661);
+    sum_of_field_test "sum of float values in computation_database.json"
+      "computation_database.json" "Types" "float" 39.3;
+    mean_of_field_test
+      "mean of float values in computation_database.json"
+      "computation_database.json" "Types" "float" 13.1;
+    computation_of_any_field_test
+      "concatenation of string values in computation_database.json"
+      "computation_database.json" "Types" "string" identity "" ( ^ )
+      "cs3110\\n";
+    computation_of_any_field_test
+      "and concatenation of bool values in computation_database.json"
+      "computation_database.json" "Types" "bool" bool_of_string true
+      ( && ) false;
+    computation_of_any_field_test
+      "or concatenation of bool values in computation_database.json"
+      "computation_database.json" "Types" "bool" bool_of_string false
+      ( || ) true;
+    computation_of_any_field_test
+      "multiplication of of int values in computation_database.json"
+      "computation_database.json" "Types" "int" int_of_string 1 ( * )
+      (-1965600);
   ]
 
 let exception_tests =
@@ -457,7 +552,7 @@ let exception_tests =
     ( "sort_field_int for test_database3.json where string cannot be \
        converted to int"
     >:: fun _ ->
-      assert_raises CannotConvertToInt (fun () ->
+      assert_raises CannotConvertToNum (fun () ->
           sort_field_int "test_database3.json" "Users" "age" compare) );
     ( "sort_field_general for test_database3.json where converting \
        function does not do a good job of converting"
@@ -465,6 +560,29 @@ let exception_tests =
       assert_raises CannotConvertElement (fun () ->
           sort_field_general "test_database3.json" "test" "2"
             bool_of_string string_of_bool compare) );
+    ( "sum_of_field for computation_database.json where values cannot \
+       be converted to floats"
+    >:: fun _ ->
+      assert_raises CannotConvertToNum (fun () ->
+          sum_of_field "computation_database.json" "Mismatch" "1") );
+    ( "mean_of_field for computation_database.json where values cannot \
+       be converted to floats"
+    >:: fun _ ->
+      assert_raises CannotConvertToNum (fun () ->
+          mean_of_field "computation_database.json" "Mismatch" "1") );
+    ( "computation_of_any_field for computation_database.json where \
+       values cannot be converted to integers"
+    >:: fun _ ->
+      assert_raises CannotConvertElement (fun () ->
+          computation_of_any_field "computation_database.json"
+            "Mismatch" "Int mismatch" int_of_string 0 ( + )) );
+    ( "computation_of_any_field for computation_database.json where \
+       values cannot be converted to integers even though they may \
+       look like they should"
+    >:: fun _ ->
+      assert_raises CannotConvertElement (fun () ->
+          computation_of_any_field "computation_database.json"
+            "Mismatch" "Max/Min" int_of_string 0 ( + )) );
   ]
 
 let suite =
@@ -474,6 +592,7 @@ let suite =
            first_section_tests;
            second_section_tests;
            third_section_tests;
+           computation_tests;
            exception_tests;
          ]
 
